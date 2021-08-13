2573787ea6e56ef5bc893a62fd2a32f571bb53a4
Tom Phillips <eflips@yahoo.com>
2021-08-13 15:37:04 -0700

    using v-for to loop through item data

--- js/main.js
+++ js/main.js
@@ -2,7 +2,21 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'A pair of warm fuzzy socks.',
-        image: './img/vmSocks-green-onWhite.jpg'
+            description: 'A pair of warm fuzzy socks.',
+            image: './img/vmSocks-green-onWhite.jpg',
+                inventory: 100,
+                inStock: true,
+                onSale: true,
+                details: ["80% cotton", "20% Polyester", "Gender-neutral"],
+                variants: [{
+                        variantId: 2234,
+                        variantColor: "green"
+                    },
+                    {
+                        variantId: 2235,
+                        variantColor: "blue"
+                    }
+                ],
+                sizes: ["small", "medium", "large"]
     }
 })
