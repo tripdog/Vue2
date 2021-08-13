1232918636428178b91a58021ea04c146e127e75
Tom Phillips <eflips@yahoo.com>
2021-08-12 23:30:55 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        description: 'A pair of warm fuzzy socks.',
+        image: './img/vmSocks-green-onWhite.jpg'
+    }
+})
