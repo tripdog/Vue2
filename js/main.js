f0423eec2addd8b0651e19d6bbd96ff03ac341e7
Tom Phillips <eflips@yahoo.com>
2021-08-13 17:06:50 -0700

    addMouseover for color variants

--- js/main.js
+++ js/main.js
@@ -9,15 +9,25 @@ var app = new Vue({
                 onSale: true,
                 details: ["80% cotton", "20% Polyester", "Gender-neutral"],
                 variants: [{
-                        variantId: 2234,
-                        variantColor: "green"
+                    variantId: 2234,
+                    variantColor: "green",
+                        variantImage: "/img/vmSocks-green-onWhite.jpg"
                     },
                     {
                         variantId: 2235,
-                        variantColor: "blue"
+                        variantColor: "blue",
+                            variantImage: "/img/vmSocks-blue-onWhite.jpg"
                     }
                 ],
         sizes: ["small", "medium", "large"],
             cart: 0
+    },
+    methods: {
+            addToCart() {
+                this.cart += 1
+            },
+            updateProduct(variantImage) {
+                this.image = variantImage
+            }
     }
 })
