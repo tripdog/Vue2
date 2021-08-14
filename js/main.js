7a2a2aacebb8d546cb222b61698f6ddc1bf00a75
Tom Phillips <eflips@yahoo.com>
2021-08-14 16:02:59 -0700

    add color style binding

--- js/main.js
+++ js/main.js
@@ -1,33 +1,37 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        product: 'Socks',
-            description: 'A pair of warm fuzzy socks.',
-            image: './img/vmSocks-green-onWhite.jpg',
-                inventory: 100,
-                inStock: true,
-                onSale: true,
-                details: ["80% cotton", "20% Polyester", "Gender-neutral"],
-                variants: [{
-                    variantId: 2234,
-                    variantColor: "green",
-                        variantImage: "/img/vmSocks-green-onWhite.jpg"
-                    },
-                    {
-                        variantId: 2235,
-                        variantColor: "blue",
-                            variantImage: "/img/vmSocks-blue-onWhite.jpg"
-                    }
-                ],
-        sizes: ["small", "medium", "large"],
-            cart: 0
+  el: "#app",
+  data: {
+    product: "Socks",
+    description: "A pair of warm fuzzy socks.",
+    image: "./img/vmSocks-green-onWhite.jpg",
+    inventory: 100,
+    inStock: true,
+    onSale: true,
+    details: ["80% cotton", "20% Polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImage: "/img/vmSocks-green-onWhite.jpg",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImage: "/img/vmSocks-blue-onWhite.jpg",
+      },
+    ],
+    sizes: ["small", "medium", "large"],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
     },
-    methods: {
-            addToCart() {
-                this.cart += 1
-            },
-            updateProduct(variantImage) {
-                this.image = variantImage
-            }
-    }
-})
+    updateProduct(variantImage) {
+      this.image = variantImage;
+    },
+    removeFromCart() {
+      this.cart -= 1;
+    },
+  },
+});
